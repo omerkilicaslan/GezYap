89de7f9135b03b796f82d6e48e833577ff2f636d
omerkilicaslan <omerrr_2001@hotmail.com>
2023-06-03 16:50:31 +0300

    Initial Commit

---
+++ GezYap/GezYapApp.swift
@@ -0,0 +1,17 @@
+//
+//  GezYapApp.swift
+//  GezYap
+//
+//  Created by Ömer Faruk Kılıçaslan on 3.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct GezYapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
